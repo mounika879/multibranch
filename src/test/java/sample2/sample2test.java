59b7293f5b76bf3d0d11d9062fa58079b5447bf8
mounika879 <48467095+mounika879@users.noreply.github.com>
2019-05-01 13:13:20 +0530

    Add files via upload

---
+++ src/test/java/sample2/sample2test.java
@@ -0,0 +1,14 @@
+package sample2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sample2test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
